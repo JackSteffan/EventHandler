1cdbf5fe3a508ed753fc82bd0cb16e090ff2099d
bdcarlson964 <150193546+bdcarlson964@users.noreply.github.com>
2024-05-02 18:21:00 -0500

    made create event and create role

--- website/urls.py
+++ website/urls.py
@@ -3,7 +3,12 @@ from . import views
 
 urlpatterns = [
     #path('', views.index, name='index'),
-    path('', views.HomeView.as_view(), name='home'),
+    path('', views.home, name='home'),
     path('events/', views.EventsView.as_view(), name='events'),
     path('profile/', views.profile, name='profile'),
+    path('event/create/', views.EventCreate.as_view(), name='event-create'),
+    path('event/<int:pk>', views.EventDetailView.as_view(), name='event-detail'),
+    path('role/create/', views.RoleCreate.as_view(), name='role-create'),
+    path('role/<uuid:pk>', views.RoleDetailView.as_view(), name='role-detail'),
 ]
+
