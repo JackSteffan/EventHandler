49621d67d070d6c0bda5ca6733f3ed01f817b732
bdcarlson964 <150193546+bdcarlson964@users.noreply.github.com>
2024-05-01 23:51:56 -0500

    added user login and admin authentication

--- website/views.py
+++ website/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
 from .models import Event
 from django.views import generic
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
+@login_required
 def index(request):
     return render(request, 'index.html')
 
+@login_required
 def home(request):
     return render(request, 'index.html')
 
-
+@login_required
 def events(request):
     # Generate counts of some of the main objects
     num_events = Event.objects.all().count()
@@ -19,8 +23,7 @@ def events(request):
     }
     return render(request, 'events.html', context=context)
 
-
-class EventsView(generic.ListView):
+class EventsView(LoginRequiredMixin, generic.ListView):
     """Generic class-based view for a list of events."""
     model = Event
     paginate_by = 10
