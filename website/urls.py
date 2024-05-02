39d3d58052d839c4fb29ddc6140e63625763b831
bdcarlson964 <150193546+bdcarlson964@users.noreply.github.com>
2024-05-01 19:17:35 -0500

    created event list

--- website/urls.py
+++ website/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('', views.home, name='home'),
-    path('events/', views.events, name='events'),
+    path('events/', views.EventsView.as_view(), name='events'),
 
 ]
